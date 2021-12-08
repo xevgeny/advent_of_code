a039ab51f946dfcedb52c4db7fcc41c3ee344be3
xevgeny <e.tskhovrebov@pm.me>
2021-12-08 07:43:15 +0100

    ok it's just median and mean

--- 2021/day7/main.jl
+++ 2021/day7/main.jl
@@ -24,20 +24,4 @@ function fuel_cost_2(pos)
 end
 
 @printf "Answer 1: %d\n" fuel_cost_1(median(N))
-
-res = optimize(fuel_cost_2, minimum(N), maximum(N), Brent())
-println(res) # print optimizer summary
-@printf "Answer 2: %d" fuel_cost_2(Int(round(res.minimizer)))
-
-#= sample output
-Answer 1: 342534
-Results of Optimization Algorithm
- * Algorithm: Brent's Method
- * Search Interval: [0.000000, 1936.000000]
- * Minimizer: 4.744910e+02
- * Minimum: 9.400409e+07
- * Iterations: 7
- * Convergence: max(|x - x_upper|, |x - x_lower|) <= 2*(1.5e-08*|x|+2.2e-16): true
- * Objective Function Calls: 8
-Answer 2: 94004208
-=#
+@printf "Answer 2: %d\n" fuel_cost_2(minimum([floor(mean(N)), ceil(mean(N))]))
