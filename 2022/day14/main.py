2c5687813d4ff33cc7b691b1b69f8d4a1744053b
xevgney <e.tskhovrebov@pm.me>
2022-12-14 10:50:52 +0100

    that will do

--- 2022/day14/main.py
+++ 2022/day14/main.py
@@ -1,56 +1,52 @@
-N = 1000
-S = (500, 0)
-
-
-def draw_line(grid, x0, y0, x1, y1):
-    if x0 == x1:  # vertical line
-        for y in range(min(y0, y1), max(y0, y1) + 1): 
-            grid[y][x0] = "#"
-    else:  # horizontal line
-        for x in range(min(x0, x1), max(x0, x1) + 1):
-            grid[y0][x] = "#"
-
-
-def draw_sand(grid, max_y):
-    x, y = S
+def add_sand(obs, max_y):  # obs - set of obstacles, sand or rocks
+    x, y = (500, 0)
     while y <= max_y:
-        if grid[y + 1][x] == "." or grid[y + 1][x - 1] == "." or grid[y + 1][x + 1] == ".":
+        if (x, y+1) not in obs:
             y += 1
-            if grid[y][x] != ".":
-                x += -1 if grid[y][x - 1] == "." else 1
+        elif (x-1, y+1) not in obs:
+            x -= 1
+            y += 1
+        elif (x+1, y+1) not in obs:
+            x += 1
+            y += 1
         else:
-            if grid[y][x] == "o":  # source is blocked
+            if (x, y) in obs:  # source is blocked
                 return False
             else:
-                grid[y][x] = "o"
+                obs.add((x, y))
                 return True
     return False
 
 
 if __name__ == "__main__":
     max_y = 0
+    rocks = set()
     input = open("input", "r").read().splitlines()
-    grid = [["." for x in range(N)] for y in range(N)]
     for line in input:
-        vecs = [[int(c) for c in vec.split(",")] for vec in line.split(" -> ")]
-        x, y = vecs[0]
+        segments = [[int(c) for c in vec.split(",")] for vec in line.split(" -> ")]
+        x, y = segments[0]
         max_y = max(max_y, y)
-        for i in range(1, len(vecs)):
-            nx, ny = vecs[i]
-            draw_line(grid, x, y, nx, ny)
+        for i in range(1, len(segments)):
+            nx, ny = segments[i]
+            if x == nx:  # vertical line
+                for yy in range(min(y, ny), max(y, ny) + 1): 
+                    rocks.add((x, yy))
+            else:  # horizontal line
+                for xx in range(min(x, nx), max(x, nx) + 1):
+                    rocks.add((xx, y))
             x, y = nx, ny
             max_y = max(max_y, y)
 
-    grid1 = [row[:] for row in grid]
+    obs1 = rocks.copy()
     it = 0
-    while draw_sand(grid1, max_y): 
+    while add_sand(obs1, max_y): 
         it += 1
     print(f"Part 1: {it}")
 
     max_y += 2
-    grid2 = [row[:] for row in grid]
-    grid2[max_y] = ["#" for _ in range(N)]
+    obs2 = rocks.copy()
+    [obs2.add((x, max_y)) for x in range(1000)]
     it = 0
-    while draw_sand(grid2, max_y): 
+    while add_sand(obs2, max_y): 
         it += 1
     print(f"Part 2: {it}")
